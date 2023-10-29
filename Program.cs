af84de958767cd8d30c0b5b51bccabc77ddda154
TaraHsani <147997426+TaraHsani@users.noreply.github.com>
2023-10-29 20:31:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UserInformationCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            string agestr = Console.ReadLine();
+            int age = int.Parse(agestr);
+            ;
+            Console.WriteLine("What is your Email adress?");
+            string Email = Console.ReadLine();
+
+            Console.WriteLine("Please writh you adress");
+            string adress = Console.ReadLine();
+
+            string discribtion = $"My name is {Name} and I'm {age} years old and my adress is {adress} ";
+            Console.WriteLine(discribtion);
+        }
+    }
+}
